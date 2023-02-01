4fa650bfefab5782e72d9a2d24503c009163278d
ahmet <ahmettutsak@hotmail.com>
2023-02-02 02:43:36 +0300

    wow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React from "react";
 
 function App() {
+  const [state, setState] = React.useState();
+  const [address, setAddress] = React.useState("");
+
+  React.useEffect(() => {
+    axios
+      .get(
+        `https://nominatim.openstreetmap.org/search?q=${address}&format=json`
+      )
+      .then((response) => {
+        setState(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, [address]);
+
+  const den = () => {
+    return state.map((dsa, index) => {
+      return (
+        <li className="border p-4 text-center" key={index}>
+          {dsa.display_name}
+        </li>
+      );
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type={"text"}
+          placeholder="City"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          className={"p-4 border rounded"}
+        />
+      </div>
+      <ul>{address && den()}</ul>
     </div>
   );
 }
