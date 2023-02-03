e41ef3c3782f1bf70f8424cebf85bf19666044b1
ahmet <ahmettutsak@hotmail.com>
2023-02-04 00:59:36 +0300

    display data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import React from "react";
 function App() {
   const [state, setState] = React.useState();
   const [address, setAddress] = React.useState("");
+  const [time, setTime] = React.useState();
+  const [stateName, setStateName] = React.useState();
+  const [show, setShow] = React.useState(false);
 
   React.useEffect(() => {
     axios
@@ -17,14 +20,59 @@ function App() {
       .catch((error) => console.error(error));
   }, [address]);
 
-  const den = () => {
-    return state.map((dsa, index) => {
+  const getWeather = async (lon, lat, stateName) => {
+    setStateName(stateName);
+    await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setTime(data.hourly);
+        setAddress("");
+      });
+    setShow(true);
+  };
+
+  const weatherGet = () => {
+    return state.map((weather, index) => {
       return (
-        <li className="border p-4 text-center" key={index}>
-          {dsa.display_name}
+        <li
+          className="border p-4 text-center cursor-pointer"
+          onClick={() =>
+            getWeather(weather.lon, weather.lat, weather.display_name)
+          }
+          key={index}
+        >
+          {weather.display_name}
         </li>
       );
     });
+  };
+
+  const showTimes = () => {
+    const times = time.time.map((items, index) => {
+      return (
+        <li className="p-4 border list-none" key={index}>
+          {items}
+        </li>
+      );
+    });
+    const degrees = time.temperature_2m.map((items, index) => {
+      return (
+        <li className="p-4 border list-none" key={index}>
+          {items}
+        </li>
+      );
+    });
+    return (
+      <div>
+        <h2>{stateName}</h2>
+        <div className="flex justify-center">
+          <div>{times}</div>
+          <div>{degrees}</div>
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -34,11 +82,15 @@ function App() {
           type={"text"}
           placeholder="City"
           value={address}
-          onChange={(e) => setAddress(e.target.value)}
+          onChange={(e) => {
+            setAddress(e.target.value);
+            setShow(false);
+          }}
           className={"p-4 border rounded"}
         />
       </div>
-      <ul>{address && den()}</ul>
+      <ul>{address.length > 0 && !show && weatherGet()}</ul>
+      <div>{show && showTimes()}</div>
     </div>
   );
 }
